58f036c3fae0bd320fcd4c8ea3af1200d24b4d2e
WillAnd <will.anderson@thought.co.uk>
2015-05-08 12:15:52 +0100

    fix caching issue

--- Areas/Admin/Services/PageViewAdminService.cs
+++ Areas/Admin/Services/PageViewAdminService.cs
@@ -68,7 +68,7 @@ namespace MrCMS.Web.Apps.Stats.Areas.Admin.Services
                 .OrderBy(Projections.CountDistinct(() => analyticsUser.Id)).Desc
                 .ThenBy(Projections.CountDistinct(() => analyticsSession.Id)).Desc
                 .ThenBy(Projections.CountDistinct(() => pageView.Id)).Desc
-                .Paged<AnalyticsPageView, PageViewResult>(Projections.CountDistinct(() => pageView.Url), query.Page);
+                .Paged<AnalyticsPageView, PageViewResult>(Projections.CountDistinct(() => pageView.Url), query.Page, enableCache:false); //todo enable cache when Nhibernate is updated to 4.1
 
             List<int?> ids = pageViewResults.Select(viewResult => viewResult.WebpageId).Where(i => i.HasValue).ToList();
             Dictionary<int, Webpage> webpages =
