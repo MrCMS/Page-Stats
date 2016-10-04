8b668fe9aba0cb5ddde3887bd9f9d1745345a075
gmstr <gary.m.strong@gmail.com>
2016-10-04 15:59:21 +0100

    Add CleanupSessions task and settings

---
+++ Settings/SessionCleanupSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MrCMS.Settings;
+
+namespace MrCMS.Web.Apps.Stats.Settings
+{
+    public class SessionCleanupSettings : SiteSettingsBase
+    {
+        public SessionCleanupSettings()
+        {
+            DaysToKeep = 90;
+            SessionsToClear = 500;
+        }
+        public int DaysToKeep { get; set; }
+
+        [Range(1, 2000)]
+        public int SessionsToClear { get; set; }
+
+        public override bool RenderInSettings
+        {
+            get { return true; }
+        }
+    }
+}
---
+++ Tasks/CleanupSessions.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using MrCMS.Entities.People;
+using MrCMS.Helpers;
+using MrCMS.Tasks;
+using MrCMS.Web.Apps.Stats.Entities;
+using MrCMS.Web.Apps.Stats.Settings;
+using MrCMS.Website;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.SqlCommand;
+
+namespace MrCMS.Web.Apps.Stats.Tasks
+{
+    public class CleanupSessions : SchedulableTask
+    {
+        private readonly IStatelessSession _session;
+        private readonly SessionCleanupSettings _settings;
+
+        public CleanupSessions(IStatelessSession session, SessionCleanupSettings settings)
+        {
+            _session = session;
+            _settings = settings;
+        }
+        public override int Priority { get { return 1; } }
+
+        protected override void OnExecute()
+        {
+            AnalyticsUser analyticsUser = null;
+            var date = CurrentRequestData.Now.AddDays(-_settings.DaysToKeep);
+            var sessions = _session.QueryOver<AnalyticsSession>()
+                .JoinAlias(x => x.AnalyticsUser, () => analyticsUser)
+                .Where(() => analyticsUser.Email == null && analyticsUser.User == null)
+                .And(x => x.CreatedOn < date)
+                .OrderBy(x => x.CreatedOn).Desc
+                .Take(_settings.SessionsToClear)
+                .List();
+
+            var ids = sessions.Select(x => x.Id).ToList();
+            var views = _session.QueryOver<AnalyticsPageView>()
+                .Where(x => x.AnalyticsSession.Id.IsIn(ids))
+                .List();
+
+            _session.Transact(session =>
+            {
+                views.ForEach(session.Delete);
+                sessions.ForEach(session.Delete);
+            });
+        }
+    }
+}
